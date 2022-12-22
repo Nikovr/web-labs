c89205e9789fbc14655ac8715c27487c3c08d6c8
Коврегин Николай Сергеевич <kol89320@hotmail.com>
2022-12-22 16:22:26 +0300

    feat: lab 6 done

---
+++ scripts/randompic.js
@@ -0,0 +1,17 @@
+function loadPic() {
+    return new Promise((resolve, reject) => {
+        var id = Math.floor(Math.random() * 5000) + 1;
+        var request = 'https://jsonplaceholder.typicode.com/photos/' + 5001;
+        fetch(request).then(response => response.json())
+        .then((json) => {
+            let preloader = document.getElementById('preloader');
+            preloader.src = json.url
+        })
+        .catch((exception) => {
+            console.log(exception)
+            let preloader = document.getElementById('preloader');
+            preloader.src = "photos/cat-explosion.gif";
+        });
+    })
+}
+
